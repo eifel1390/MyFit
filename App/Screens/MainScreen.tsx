4b70126affcf8bc0078c2d3e0f5d668e3385912e
maksimturkin <maksimturkin50@gmail.com>
2022-10-30 15:40:26 +0200

    add library for download image

--- App/Screens/MainScreen.tsx
+++ App/Screens/MainScreen.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import {launchImageLibrary} from 'react-native-image-picker';
 
 export const MainScreen: () => JSX.Element = () => {
+  const handlePressButton = async () => {
+    const result = await launchImageLibrary({
+      mediaType: 'photo',
+    });
+    console.log('result is ', JSON.stringify(result));
+  };
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => {
-          console.log('press button');
-        }}>
+      <TouchableOpacity style={styles.button} onPress={handlePressButton}>
         <Text style={styles.text}>{'Download video'}</Text>
       </TouchableOpacity>
     </View>
